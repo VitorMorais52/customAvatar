a4ed5d87171b9b97af1fb9ebfb52eb2ea7f4b911
Morais <vitormorais52@gmail.com>
2024-01-27 17:05:26 -0300

    reorganize components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import AssembledBody from "./AssembledBody";
-import ColorPicker from "./AssembledBody/ColorPicker";
-import Options from "./Options";
 import NavBar from "./NavBar";
 import * as AllHair from "./AssembledBody/BodyParts/hair";
 import * as AllEyebrowAndNose from "./AssembledBody/BodyParts/eyebrowAndNose";
@@ -22,60 +20,22 @@ import "./App.css";
 
 function App() {
   const [currentTab, setCurrentTab] = useState("hair");
-  const [colorSettings, setColorSettings] = useState({
-    hair: "#4D191A",
-    skin: "#7C533E",
-    background: "",
-    tshirt: "#442178",
-  });
-  const [partsSettings, setPartsSettings] = useState({
-    hair: "one",
-    eyebrowAndNose: "one",
-    mouth: "one",
-  });
-
-  const changeColorSettings = (colorKey, value) =>
-    setColorSettings({ ...colorSettings, [colorKey]: value });
-
-  const setConfigs = (value) =>
-    setPartsSettings({ ...partsSettings, [currentTab]: value });
 
   return (
     <div className="container">
-      <div className="content" style={{ display: "none" }}>
-        <AssembledBody
-          partsSettings={partsSettings}
-          colorSettings={colorSettings}
-        />
+      <div className="content">
+        <AssembledBody currentTab={currentTab} tabsConfig={tabsConfig} />
         <h2>Editar</h2>
         <NavBar
           tabsConfig={tabsConfig}
           currentTab={currentTab}
           changeCurrentTab={(v) => setCurrentTab(v)}
         />
-        {["skin", "tshirt", "hair"].includes(currentTab) && (
-          <div className="colorPickerWrapper">
-            <ColorPicker
-              type={currentTab === "hair" ? "slider" : ""}
-              colorKey={currentTab}
-              currentColor={colorSettings.hair}
-              changeCurrentColor={(value) =>
-                changeColorSettings(currentTab, value)
-              }
-            />
-          </div>
-        )}
-        <Options
-          tabsConfig={tabsConfig}
-          currentTab={currentTab}
-          partsSettings={partsSettings}
-          changeConfigs={(v) => setConfigs(v)}
-        />
       </div>
       <div
         id="circle"
         className="demo"
-        style={{ position: "relative", width: "200px" }}
+        style={{ display: "none", position: "relative", width: "200px" }}
       >
         <svg viewBox="0 0 100 100">
           <path
